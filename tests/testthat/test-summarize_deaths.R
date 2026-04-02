7f855043924e26629b1c5707324980a0de4718ac
Caleb Peterson <pet22063@byui.edu>
2026-04-02 12:10:25 -0600

    added additional fucntionality to summarize_deaths to compare across years

--- tests/testthat/test-summarize_deaths.R
+++ tests/testthat/test-summarize_deaths.R
@@ -61,3 +61,35 @@ test_that("summarize_deaths returns a tibble", {
     expect_s3_class(result, "data.frame")
     expect_true(tibble::is_tibble(result))
 })
+
+test_that("summarize_deaths compares multiple years", {
+    df1 <- data.frame(sex = c("Male", "Female", "Male"))
+    df2 <- data.frame(sex = c("Female", "Female", "Male"))
+
+    result <- summarize_deaths(y1 = df1, y2 = df2, by = "sex")
+
+    expect_true("year" %in% colnames(result))
+    expect_equal(sort(unique(result$year)), c("y1", "y2"))
+    expect_equal(result$n[result$year == "y1" & result$sex == "Male"], 2)
+    expect_equal(result$n[result$year == "y2" & result$sex == "Female"], 2)
+})
+
+test_that("summarize_deaths errors when multi-year datasets are unnamed", {
+    df1 <- data.frame(sex = c("Male", "Female"))
+    df2 <- data.frame(sex = c("Male", "Female"))
+
+    expect_error(
+        summarize_deaths(df1, df2, by = "sex"),
+        "When providing multiple datasets, all must be named"
+    )
+})
+
+test_that("summarize_deaths errors when multi-year dataset has missing column", {
+    df1 <- data.frame(sex = c("Male", "Female"))
+    df2 <- data.frame(race = c("White", "Black"))
+
+    expect_error(
+        summarize_deaths(y1 = df1, y2 = df2, by = "sex"),
+        "The following columns were not found in 'y2': sex"
+    )
+})
